d37582d55a7c4a44b9b1eb7613c727c0fea8da8b
Rainboy <597872644@qq.com>
2016-12-20 14:12:30 +0800

    [add] config.js文件

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Rainboy的blog
 ## logs
 
  - [ ] 建立前端页面
+
+## How To Use
+
+```sh
+cp _config.js config.js
+# 配置文件
+```
+
+
---
+++ _config.js
@@ -0,0 +1,7 @@
+// 这里是配置文件
+//
+
+module.exports = {
+  remote:'https://github.xx', //远程地址
+  branch_blog:'master',//文本的地址
+}
